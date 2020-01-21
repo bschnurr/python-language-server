777cec14f7ad69598ac95718b14ada64a5f5d2e8
Bill Schnurr <bschnurr@microsoft.com>
2020-01-21 10:46:57 -0800

    inital porting of LSC testing code from PTVS to MPLS new code is mostly contained in UnitTests.LanguageServerClient project wip porting a single test and still configuring server launching.

--- src/LanguageServer/Test/AssemblySetup.cs
+++ src/LanguageServer/Test/AssemblySetup.cs
@@ -15,19 +15,40 @@
 
 using Microsoft.Python.Core.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Threading;
+using System;
+using System.Collections.Generic;
 using TestUtilities;
+using UnitTests.LanguageServerClient;
 
 namespace Microsoft.Python.LanguageServer.Tests {
     [TestClass]
     public sealed class AssemblySetup {
         [AssemblyInitialize]
-        public static void Initialize(TestContext testContext) => AnalysisTestEnvironment.Initialize();
+        public static void Initialize(TestContext testContext) => LanguageServerTestEnvironment.Initialize(testContext);
 
         private class AnalysisTestEnvironment : TestEnvironmentImpl, ITestEnvironment {
             public static void Initialize() {
                 var instance = new AnalysisTestEnvironment();
                 Instance = instance;
                 TestEnvironment.Current = instance;
+            }
+        }
+
+        private class LanguageServerTestEnvironment : TestEnvironmentImpl, ITestEnvironment {
+            public static JoinableTaskContext JoinableTaskContext;
+
+            public static void Initialize(TestContext testContext) {
+                var instance = new LanguageServerTestEnvironment();
+                Instance = instance;
+                TestEnvironment.Current = instance;
+
+                var serverFolderPath = testContext.Properties["TestDeploymentDir"].ToString();
+
+                //PythonLanguageServerDotNetCore.ExtractToFolder(serverFolderPath);
+
+                Environment.SetEnvironmentVariable("PTVS_NODE_SERVER_ENABLED", "0");
+                Environment.SetEnvironmentVariable("PTVS_DOTNETCORE_SERVER_LOCATION", serverFolderPath);
             }
         }
     }
